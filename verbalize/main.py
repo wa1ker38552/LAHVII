0aa7535c3dbe5519408384a7a2900feb1c5b8dea
walker <100868154+wa1ker38552@users.noreply.github.com>
2023-04-09 04:26:37 -0700

    Update main.py

--- verbalize/main.py
+++ verbalize/main.py
@@ -11,13 +11,13 @@ import twilio
 import json
 import time
 import gtts
+import sys
 import os
 
-
-DEEPGRAM_API_KEY = ""
-token = ''
-account_sid = ''
-auth_token = ''
+DEEPGRAM_API_KEY = "77e029c585bfcf55caa78ce17f073bd2fe933a49"
+token = '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'
+account_sid = 'ACf0a27a867c2a3913ff1901c2a0908d0c'
+auth_token = 'bda8e5e2c9890eea4a877fc54669a5e5'
 
 model = 'text-davinci-003'
 command_key = 'hey'
@@ -101,97 +101,121 @@ async def microphone():
     while stream.is_active():
         await asyncio.sleep(0.1)
 
-    stream.stop_stream()
-    stream.close()
+    # stream.stop_stream()
+    # stream.close()
 
+async def sender(ws):
+    try:
+        while True:
+            data = await audio_queue.get()
+            await ws.send(data)
+    except Exception as e: pass
+
+async def receiver(ws):
+    global incoming_command, command_key, x
+    async for msg in ws:
+        msg = json.loads(msg)
+        try:
+            transcript = msg['channel']['alternatives'][0]['transcript']
+        except KeyError:
+            transcript = ''
+
+        if transcript:
+            if incoming_command:
+                if 'applications' in transcript.lower() and 'open' in transcript.lower():
+                    apps = get_applications()
+                    speak('and '.join(apps)+' are open right now')
+
+                elif 'save contact' in transcript.lower():
+                    number = replace_numbers(' '.join(transcript.lower().split()[2:-2]))
+                    name = transcript.lower().split()[-1]
+
+                    data = json.loads(open('contacts.json', 'r').read())
+                    if not name in data:
+                        data[name] = number
+                        with open('contacts.json', 'w') as file:
+                            file.write(json.dumps(data, indent=2))
+                        speak(f'Saved the contact {name} as {number}')
+                    else:
+                        speak(f'{name} is already saved in contacts')
+
+                elif 'delete contact' in transcript.lower():
+                    data = json.loads(open('contacts.json', 'r').read())
+                    name = transcript.lower().split()[-1]
+                    if name in data:
+                        del data[name]
+                        with open('contacts.json', 'w') as file:
+                            file.write(json.dumps(data, indent=2))
+                        speak(f'Deleted the contact {name}')
+                    else:
+                        speak(f'{name} isnt saved in contacts')
+
+                elif 'text' in transcript.lower():
+                    target = transcript.lower().split("text")[-1][1:]
+                    data = json.loads(open('contacts.json', 'r').read())
+                    if target in data:
+                        name = target
+                        target = data[target]
+                        speak(f'What message do you want to send to {name}?')
+                    else:
+                        speak(f'What message do you want to send to {target}?')
+                    print('Listening for message...')
+
+                    rec = sr.Recognizer()
+                    with sr.Microphone() as source:
+                        audio = rec.record(source, duration=3)
+                        text = rec.recognize_google(audio, language='en-IN', show_all=True)
+                    target = replace_numbers(target)
+
+                    try:
+                        send_text('+1'+target, text['alternative'][0]['transcript'])
+                        speak(f"Sent the message {text['alternative'][0]['transcript']} to {target}")
+                    except twilio.base.exceptions.TwilioRestException:
+                        speak(f"There was an error with the phone number {target}")
+                    except KeyError:
+                        speak("Sorry I didn't understand that, try using the command again")
+                    except TypeError:
+                        speak("Sorry I didn't understand that, try using the command again")
+                    incoming_command = False
+                    os.startfile(sys.argv[0])
+                    sys.exit()
+
+                elif 'show' in transcript.lower() and 'images' in transcript.lower():
+                    query = ' '.join(transcript.lower().split()[transcript.lower().split().index('of')+1:])
+                    webbrowser.get().open(f'https://www.google.com/search?q={query}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjN5NWMz5v-AhUgADQIHeYPCAIQ0pQJegQIBRAC&biw=1517&bih=690&dpr=0.9')
+                    speak(f'Showing images of {query}')
+
+                elif 'search' in transcript.lower():
+                    webbrowser.get().open(f'https://google.com/search?q='+transcript.lower().replace('search', '').replace('for', ''))
+                    speak('Finished searching')
+
+                elif 'open' in transcript.lower() and 'website' in transcript.lower():
+                    webbrowser.get().open(f'https://{transcript.lower().split()[-1]}.com')
+                    speak('Finished opening website')
+                else:
+                    print('Querying GPT')
+                    c = completions(transcript)
+                    speak(c)
+                    print('finished speaking')
+
+                incoming_command = False
+            else:
+                if command_key in transcript.lower():
+                    incoming_command = True
+                    print('Listening to command...')
+                    mixer.init()
+                    mixer.music.load(f'chime.mp3')
+                    mixer.music.play()
+                    while mixer.music.get_busy(): continue
 
 async def process():
     extra_headers = {'Authorization': 'token '+DEEPGRAM_API_KEY}
-
     async with websockets.connect('wss://api.deepgram.com/v1/listen?encoding=linear16&sample_rate=16000&channels=1', extra_headers=extra_headers) as ws:
-        async def sender(ws):
-            try:
-                while True:
-                    data = await audio_queue.get()
-                    await ws.send(data)
-            except Exception as e: pass
-
-        async def receiver(ws):
-            global incoming_command, command_key, x
-            async for msg in ws:
-                msg = json.loads(msg)
-                try:
-                    transcript = msg['channel']['alternatives'][0]['transcript']
-                except KeyError:
-                    transcript = ''
-
-                if transcript:
-                    if incoming_command:
-                        if 'applications' in transcript.lower() and 'open' in transcript.lower():
-                            apps = get_applications()
-                            speak('and '.join(apps)+' are open right now')
-
-                        elif 'text' in transcript.lower():
-                            target = f'+1{transcript.lower().split("text")[-1]}'
-                            speak(f'What message do you want to send to {target.replace("+1", "")}?')
-                            time.sleep(5)
-
-                            rec = sr.Recognizer()
-                            with sr.Microphone() as source:
-                                audio = rec.record(source, duration=3)
-                                text = rec.recognize_google(audio, language='en-IN', show_all=True)
-                            target = replace_numbers(target)
-
-                            try:
-                                send_text(target, text['alternative'][0]['transcript'])
-                                speak(f"Sent the message {text['alternative'][0]['transcript']} to {target}")
-                            except twilio.base.exceptions.TwilioRestException:
-                                speak(f"There was an error with the phone number {target}")
-                            except KeyError:
-                                speak("Sorry I didn't understand that, try using the command again")
-                            except TypeError:
-                                speak("Sorry I didn't understand that, try using the command again")
-
-                        elif 'show' in transcript.lower() and 'images' in transcript.lower():
-                            query = ' '.join(transcript.lower().split()[transcript.lower().split().index('of')+1:])
-                            webbrowser.get().open(f'https://www.google.com/search?q={query}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjN5NWMz5v-AhUgADQIHeYPCAIQ0pQJegQIBRAC&biw=1517&bih=690&dpr=0.9')
-                            speak(f'Showing images of {query}')
-
-                        elif 'search' in transcript.lower():
-                            webbrowser.get().open(f'https://google.com/search?q='+transcript.lower().replace('search', '').replace('for', ''))
-                            speak('Finished searching')
-
-                        elif 'open' in transcript.lower() and 'website' in transcript.lower():
-                            webbrowser.get().open(f'https://{transcript.lower().split()[-1]}.com')
-                            speak('Finished opening website')
-                        else:
-                            print('Querying GPT')
-                            c = completions(transcript)
-                            speak(c)
-                            print('finished speaking')
-
-                        incoming_command = False
-                    else:
-                        if command_key in transcript.lower():
-                            incoming_command = True
-                            print('Listening to command...')
-                            mixer.init()
-                            mixer.music.load(f'chime.mp3')
-                            mixer.music.play()
-                            while mixer.music.get_busy(): continue
-
         await asyncio.gather(sender(ws), receiver(ws))
 
 async def run():
     await asyncio.gather(microphone(), process())
 
 if __name__ == '__main__':
-    running = False
-    while True:
-        try:
-            if not running:
-                running = True
-                asyncio.run(run())
-        except websockets.exceptions.ConnectionClosedError:
-            print('Connection closed unexpectedly! (GPT took longer than 20 seconds to respond)')
-            running = False
+    # 650 898 3822
+    asyncio.run(run())
